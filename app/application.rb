90010f80748112cbdef70bafdac393c4a6d7aace
Eric <eric.m.lombardo@gmail.com>
2021-01-29 20:46:24 -0500

    commit tree dff758aa21eb10314bf31eaa6664829bf91c797b

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+      item_name = req.path.split(/items\//).last
+      item_price =  @@items.detect {|item| item.name == item_name}
+      if item_price.nil?
+        resp.write "Item not found"
+        resp.status = 400
+      else 
+        resp.write "#{item_price.price}"
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
